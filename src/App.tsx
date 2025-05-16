da8f744114b5c2b32d4f8a31bf47bbb77e12859a
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2025-05-16 19:08:06 -0300

    feat: adjusts in colors and wallpaper image with disposition items

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import { GenerateTermForm } from "./components/terms-palm-form/generate-term-for
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen font-nunito">
-      <h1 className="text-2xl font-extrabold mb-4">Gerador de Termo de Responsabilidade</h1>
+      <span className="flex flex-col items-center justify-center">
+        <img
+          src="/alvoar.png"
+          alt="Logo"
+          className="w-16 h-16 mb-4"
+        />
+        <h1 className="text-2xl font-extrabold mb-4 text-2xl text-white">Gerador de Termo de Responsabilidade</h1>
+      </span>
       <GenerateTermForm />
       <Toaster />
     </div>
--- src/components/terms-palm-form/generate-term-form.tsx
+++ src/components/terms-palm-form/generate-term-form.tsx
@@ -14,7 +14,6 @@ import {
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -144,178 +143,180 @@ export function GenerateTermForm() {
 
   return (
     <Form {...generateTermForm}>
-      <form onSubmit={generateTermForm.handleSubmit(handleSubmit)} className="space-y-8">
-        <FormField
-          control={generateTermForm.control}
-          name="employee"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="font-bold">Funcionário</FormLabel>
-              <FormControl>
-                <Input placeholder="Insira o nome completo do funcionário" {...field}></Input>
-              </FormControl>
-              <FormDescription>
-                Nome completo do funcionário que irá entregar o dispositivo.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={generateTermForm.control}
-          name="cpf"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="font-bold">CPF</FormLabel>
-              <FormControl>
-                <Input placeholder="Insira o CPF do funcionário" {...field}></Input>
-              </FormControl>
-              <FormDescription>CPF do funcionário que irá entregar o dispositivo.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={generateTermForm.control}
-          name="deviceModel"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="font-bold">Modelo do Dispositivo</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button variant="outline" role="combobox" className={
-                      cn("w-[200px] justify-between", !field.value && "text-muted-foreground")}>
-                      {field.value
-                        ? DeviceModel.find((device) => device.value === field.value)?.label
-                        : "Selecionar modelo..."}
-                      <LuChevronsUpDown className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput placeholder="Procurar modelo..." className="h-9" />
-                    <CommandList>
-                      <CommandEmpty>Modelo não encontrado.</CommandEmpty>
-                      <CommandGroup>
-                        {DeviceModel.map((device) => (
-                          <CommandItem
-                            key={device.id}
-                            value={device.value}
-                            onSelect={() => {
-                              generateTermForm.setValue("deviceModel", device.value)
-                            }}
-                          >
-                            {device.label}
-                            <LuCheck
-                              className={cn(
-                                "ml-auto",
-                                device.value === field.value ? "opacity-100" : "opacity-0"
-                              )}
-                            />
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <FormDescription>Modelo do dispositivo que será entregue.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )
-          }
-        />
-        < FormField
-          control={generateTermForm.control}
-          name="imeiSerialDevice"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="font-bold">IMEI/Série do Dispositivo</FormLabel>
-              <FormControl>
-                <Input placeholder="Insira o IMEI/Série do dispositivo" {...field}></Input>
-              </FormControl>
-              <FormDescription>IMEI ou número de série do dispositivo que será entregue.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        < FormField
-          control={generateTermForm.control}
-          name="componentsComputer"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormLabel className="font-bold">Componentes</FormLabel>
-              </div>
-              {ComponentsComputerOptions.map((component) => (
-                <FormField
-                  key={component.id}
-                  control={generateTermForm.control}
-                  name="componentsComputer"
-                  render={({ field }) => (
-                    <FormItem key={component.id} className="flex flex-row items-start space-x-3 space-y-0">
-                      <FormControl>
-                        <Checkbox
-                          checked={field.value?.includes(component.id)}
-                          onCheckedChange={(checked) => {
-                            return checked ? field.onChange([...field.value, component.id]) : field.onChange(field.value?.filter((item) => item !== component.id));
-                          }}
-                        ></Checkbox>
-                      </FormControl>
-                      <FormLabel className="text-sm font-normal">
-                        {component.label}
-                      </FormLabel>
-                    </FormItem>
-                  )}
-                ></FormField>
+      <form onSubmit={generateTermForm.handleSubmit(handleSubmit)} className="space-y-8 p-8 bg-slate-50 rounded-lg shadow-sm border-none md:w-[800px] lg:w-[1000px] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <FormField
+        control={generateTermForm.control}
+        name="employee"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="font-bold">Funcionário</FormLabel>
+            <FormControl>
+          <Input placeholder="Insira o nome completo do funcionário" {...field}></Input>
+            </FormControl>
+            {/* <FormDescription>
+          Nome completo do funcionário que irá entregar o dispositivo.
+            </FormDescription> */}
+            <FormMessage />
+          </FormItem>
+        )}
+          />
+          <FormField
+        control={generateTermForm.control}
+        name="cpf"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="font-bold">CPF</FormLabel>
+            <FormControl>
+          <Input placeholder="Insira o CPF do funcionário" {...field}></Input>
+            </FormControl>
+            {/* <FormDescription>CPF do funcionário que irá entregar o dispositivo.</FormDescription> */}
+            <FormMessage />
+          </FormItem>
+        )}
+          />
+          <FormField
+        control={generateTermForm.control}
+        name="deviceModel"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="font-bold">Modelo do Dispositivo</FormLabel>
+            <Popover>
+          <PopoverTrigger asChild>
+            <FormControl>
+              <Button variant="outline" role="combobox" className={
+            cn("w-[200px] justify-between", !field.value && "text-muted-foreground")}>
+            {field.value
+              ? DeviceModel.find((device) => device.value === field.value)?.label
+              : "Selecionar modelo..."}
+            <LuChevronsUpDown className="ml-auto h-4 w-4 opacity-50" />
+              </Button>
+            </FormControl>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Procurar modelo..." className="h-9" />
+              <CommandList>
+            <CommandEmpty>Modelo não encontrado.</CommandEmpty>
+            <CommandGroup>
+              {DeviceModel.map((device) => (
+                <CommandItem
+              key={device.id}
+              value={device.value}
+              onSelect={() => {
+                generateTermForm.setValue("deviceModel", device.value)
+              }}
+                >
+              {device.label}
+              <LuCheck
+                className={cn(
+                  "ml-auto",
+                  device.value === field.value ? "opacity-100" : "opacity-0"
+                )}
+              />
+                </CommandItem>
               ))}
-            </FormItem>
-          )}
-        />
-        < FormField
-          control={generateTermForm.control}
-          name="isBrokenScreen"
-          render={() => (
-            <div>
-              <h3 className="mb-4 text-lg font-bold">Informações Adicionais</h3>
-              <div className="space-y-4">
-                <FormField
-                  control={generateTermForm.control}
-                  name="isBrokenScreen"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                      <div className="space-y-0.5">
-                        <FormLabel className="font-bold">
-                          <FaMobileScreen className="mr-2 inline-block" />
-                          Tela Quebrada
-                        </FormLabel>
-                        <FormDescription>
-                          Se o dispositivo tem a tela quebrada, selecione esta opção.
-                        </FormDescription>
-                      </div>
-                      <FormControl>
-                        <Switch
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
+            </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+            </Popover>
+            {/* <FormDescription>Modelo do dispositivo que será entregue.</FormDescription> */}
+            <FormMessage />
+          </FormItem>
+        )
+        }
+          />
+          <FormField
+        control={generateTermForm.control}
+        name="imeiSerialDevice"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="font-bold">IMEI/Série do Dispositivo</FormLabel>
+            <FormControl>
+          <Input placeholder="Insira o IMEI/Série do dispositivo" {...field}></Input>
+            </FormControl>
+            {/* <FormDescription>IMEI ou número de série do dispositivo que será entregue.</FormDescription> */}
+            <FormMessage />
+          </FormItem>
+        )}
+          />
+          <FormField
+        control={generateTermForm.control}
+        name="componentsComputer"
+        render={() => (
+          <FormItem>
+            <div className="mb-4">
+          <FormLabel className="font-bold">Componentes</FormLabel>
             </div>
-          )}
-        />
-        <div className="flex justify-between">
-          <Button type="submit" className="hover:cursor-pointer bg-emerald-700 hover:bg-emerald-600 transition-all duration-300">
-            <IoDocumentText className="mr-2" />
-            Gerar Termo
+            {ComponentsComputerOptions.map((component) => (
+          <FormField
+            key={component.id}
+            control={generateTermForm.control}
+            name="componentsComputer"
+            render={({ field }) => (
+              <FormItem key={component.id} className="flex flex-row items-start space-x-3 space-y-0">
+            <FormControl>
+              <Checkbox
+                checked={field.value?.includes(component.id)}
+                onCheckedChange={(checked) => {
+              return checked ? field.onChange([...field.value, component.id]) : field.onChange(field.value?.filter((item) => item !== component.id));
+                }}
+              ></Checkbox>
+            </FormControl>
+            <FormLabel className="text-sm font-normal">
+              {component.label}
+            </FormLabel>
+              </FormItem>
+            )}
+          ></FormField>
+            ))}
+          </FormItem>
+        )}
+          />
+          <FormField
+        control={generateTermForm.control}
+        name="isBrokenScreen"
+        render={() => (
+          <div>
+            <h3 className="mb-4 text-lg font-bold">Informações Adicionais</h3>
+            <div className="space-y-4">
+          <FormField
+            control={generateTermForm.control}
+            name="isBrokenScreen"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+            <div className="space-y-0.5">
+              <FormLabel className="font-bold">
+                <FaMobileScreen className="mr-2 inline-block" />
+                Tela Quebrada
+              </FormLabel>
+              {/* <FormDescription>
+                Se o dispositivo tem a tela quebrada, selecione esta opção.
+              </FormDescription> */}
+            </div>
+            <FormControl>
+              <Switch
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+            </FormControl>
+              </FormItem>
+            )}
+          />
+            </div>
+          </div>
+        )}
+          />
+        </div>
+        <div className="flex justify-end gap-4">
+          <Button type="submit" className="hover:cursor-pointer bg-[#010a41] hover:bg-[#2a3050] transition-all duration-300">
+        <IoDocumentText className="mr-2" />
+        Gerar Termo
           </Button >
           <Button type="button" className="hover:cursor-pointer bg-red-800 hover:bg-red-700 transition-all duration-300" onClick={handleResetFields}>
-            <GrPowerReset className="mr-2" />
-            Reiniciar
+        <GrPowerReset className="mr-2" />
+        Reiniciar
           </Button >
         </div>
       </form >
